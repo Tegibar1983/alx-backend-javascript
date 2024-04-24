83bba67d05d49d1186aa0879153fa2da2f9e694f
Tegibar1983 <tbtf1991@gmail.com>
2024-04-24 05:42:12 +0000

    ES6 Promises

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,8 @@
 export default function divideFunction(numerator, denominator){
-	if (denominator === 0){
-		throw error('cannot divide by 0');
+	if (denominator === 0) {
+		throw Error('can not divide by 0');
+
+	}else{
+	return numerator/denominator
 	}
-	return numerator / denominator;
 }
