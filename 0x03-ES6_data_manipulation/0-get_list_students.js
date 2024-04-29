4f15fabcba9aecb8c47c9fd28c414728d71b7600
Tegibar1983 <tbtf1991@gmail.com>
2024-04-29 04:57:47 +0000

    ES6 Data Manipulations

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * A function that retrive student lists
+ * Returned data will be id, first name & location */
+
+export default function getListStudents(){
+	return [
+		{id:1, firstName:'Guillaume', location:'San Francisco'},
+		{id:2, firstName:'James', location:'Columbia'},
+		{id:5, firstName:'Serena', location:'San Francisco'}
+	];
+}
