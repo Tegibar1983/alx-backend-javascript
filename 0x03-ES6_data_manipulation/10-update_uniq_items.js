af02908e1199d0f8dfc6e1ea12969b0c5c884ab2
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 14:29:39 +0000

    10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+/**
+ * A function that returns updated map for a list
+ */
+
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+  throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+	 if (value === 1) {
+		 map.set(key, 100);
+	 }
+  });
+}
