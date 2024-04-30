f44a8c71a5029add40c1ea6bf5801e5d287b1d34
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 11:28:25 +0000

    ES6 DATA Manipulation

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,13 @@
+/**
+ * A func that retrives sum of IDs fom z list
+ */
+
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
