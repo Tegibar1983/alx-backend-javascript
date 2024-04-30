2aade7c467d379a27ac3d5a6a2b1d80c5db314a7
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 13:58:04 +0000

    ES6 DATA MANIPULATION

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Retrives map and quanity of grocery names
+ */
+
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+	return new Map(values);
+ 
+}
