af8cd52325b743fc141eaf9dce143c4f85f16e41
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 10:55:17 +0000

    ES6 DATA MANIPULATIONS

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,11 +2,10 @@
  * A function that retrives student IDs as array
  */
 
-export default function getListStudentIds(students
-){
-	if(students instanceof Array){
-		return students.map((student) => (student.id));
-	}
 
-	return [];
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
