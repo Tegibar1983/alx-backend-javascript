6afe131d501570ea8f07f9b94ae3c13b20c317bf
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 11:11:40 +0000

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/**
+ * A function that returns student city location
+ */
+
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
