119990fbd339f208924aa587f3a585d148d274a0
Tegibar1983 <tbtf1991@gmail.com>
2024-04-30 13:16:46 +0000

    ES6 DATA MAN

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,6 +2,6 @@
  * A function that returns a boolean if exists
  */
 
-export default function hasValuesFromArray(set, array){
+export default function hasValuesFromArray(set, array) {
   return array.every((value) => set.has(value));
 }
